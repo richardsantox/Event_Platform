7b7ef1d6165bb51c3ca34be6ff6bb024d6740b08
richardsantox <richardsantospereira4322@gmail.com>
2022-06-30 13:21:51 -0300

    [Criação do projeto] {30/06/2022  13:21}

---
+++ src/components/Lesson.tsx
@@ -0,0 +1,21 @@
+export function Lesson(){
+    return (
+        <a href="">
+            <span className="text-gray-300">
+                Terça • 22 de junho • 19h00
+            </span>
+
+            <div className="rounded border border-gray-500 p-4 mt-2">
+                <header>
+                    <span>
+                        Conteúdo liberado
+                    </span>
+                    <span>
+                        AO VIVO
+                    </span>
+                </header>
+                <strong>Abertura do evento Ignite Lab</strong>
+            </div>
+        </a>
+    )
+}
