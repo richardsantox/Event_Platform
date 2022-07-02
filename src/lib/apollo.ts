5b0eee88353c652e9bb35b0c0862b1829e7e0e5f
richardsantox <richardsantospereira4322@gmail.com>
2022-07-02 20:46:17 -0300

    [Roteamento e player de aulas] + [Inscrição via GraphQl]

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4sq9t982fko01wg2a3lbxng/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
