003d4d325f67d9e14a0f334dac9aba8b1e390a2a
richardsantox <richardsantospereira4322@gmail.com>
2022-07-05 11:53:59 -0300

    @richardsantox [resolvendo bug de links]

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -74,7 +74,7 @@ export function Video(props: VideoProps){
                 </div>
 
                 <div className="gap-8 mt-20 grid grid-cols-2"> 
-                    <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+                    <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
                         <div className="bg-green-700 h-full p-6 flex items-center">
                             <FileArrowDown size={40} />
                         </div>
@@ -89,7 +89,7 @@ export function Video(props: VideoProps){
                         </div>
                     </a>
 
-                    <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+                    <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
                         <div className="bg-green-700 h-full p-6 flex items-center">
                             <FileArrowDown size={40} />
                         </div>
